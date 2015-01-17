e04740bb77ca2e6fc776406682d44c12d69840ce
yu zhang <lacozhang@gmail.com>
2015-01-17 21:35:19 +0800

    first checkin of R programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,25 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## retuning a object with four different functions.
 makeCacheMatrix <- function(x = matrix()) {
 
+	inversionMatObj <- NULL
+
+	set <- function(matObj){
+		x <<- matObj
+		inversionMatObj <<- NULL
+	}
+
+	get <- function() x
+
+	setinverseion <- function(invMatObj){
+		inversionMatObj <<- invMatObj
+	}
+
+	getinversion <- function() inversionMatObj
+
+	list(set = set, get=get, setinverse=setinverseion, getinverse=getinversion)
 }
 
 
@@ -12,4 +28,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+        invObj <- x$getinverse()
+        if(!is.null(invObj)){
+        	message('get cached inverse of matrix')
+        	return(invObj)
+        }
+
+        dat <- x$get()
+        invObj <- solve(dat)
+        x$setinverse(invObj)
 }
